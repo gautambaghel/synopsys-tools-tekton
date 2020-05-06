bd292df37dae5b337431f532552fe9f994c82d2c
Gautam <>
2020-05-06 05:09:44 -0400

    accomodate APIs changes, pipelines run sucessfully

--- synopsys/polaris/entrypoint.sh
+++ synopsys/polaris/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export POLARIS_HOME="/polaris"
+export POLARIS_HOME="/workspace/polaris"
 SCRIPT_ARGS="$@"
 POLARIS_ARGS=""
 
@@ -9,7 +9,8 @@ for i in $*; do
     export POLARIS_SERVER_URL=`echo ${i} | cut -d'=' -f 2`
     LOGGABLE_SCRIPT_ARGS="$LOGGABLE_SCRIPT_ARGS $i"
   elif [[ $i == --polaris.config=* ]]; then
-    export POLARIS_CONFIG_FILENAME=`echo ${i} | cut -d'=' -f 2`
+    POLARIS_CONFIG_FILENAME=`echo ${i} | cut -d'=' -f 2`
+    export POLARIS_CONFIG_FILENAME=`echo -c $POLARIS_CONFIG_FILENAME`
     LOGGABLE_SCRIPT_ARGS="$LOGGABLE_SCRIPT_ARGS $i"
   elif [[ $i == --polaris.access.token=* ]]; then
     export POLARIS_ACCESS_TOKEN=`echo ${i} | cut -d'=' -f 2`
@@ -23,4 +24,5 @@ for i in $*; do
 done
 
 echo "running Polaris at ${POLARIS_HOME}/cli/polaris with env variables ${LOGGABLE_SCRIPT_ARGS}"
-eval "polaris analyze ${POLARIS_ARGS}"
+echo "running command ~> polaris ${POLARIS_CONFIG_FILENAME} analyze ${POLARIS_ARGS}"
+eval "polaris ${POLARIS_CONFIG_FILENAME} analyze ${POLARIS_ARGS}"
